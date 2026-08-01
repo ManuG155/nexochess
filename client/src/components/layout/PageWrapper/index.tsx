31db610c65a9dec9f0c26f2f66a1fb3948203540
Manuel García Villaescusa <manuel.garcia.villaescusa@gmail.com>
2026-08-01 10:42:15 +0200

    Carga las correcciones de contraste tras el tema global

--- client/src/components/layout/PageWrapper/index.tsx
+++ client/src/components/layout/PageWrapper/index.tsx
@@ -14,6 +14,7 @@ import PageWrapperProps from "./PageWrapperProps";
 import * as styles from "./PageWrapper.module.css";
 import "./GlobalTheme.css";
 import "./GlobalThemePolish.css";
+import "./LightThemeContrast.css";
 
 const queryClient = new QueryClient();
 
