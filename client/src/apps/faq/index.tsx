8d4e4982879a2baebf51cab51303f8e8adc9dc70
Manuel García Villaescusa <manuel.garcia.villaescusa@gmail.com>
2026-08-06 16:01:55 +0200

    Añade la entrada React de la FAQ

---
+++ client/src/apps/faq/index.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+
+import I18nGate from "@/components/layout/I18nGate";
+import PageWrapper from "@/components/layout/PageWrapper";
+import { removeDefaultConsentLink } from "@/lib/consent";
+
+import Faq from "./Faq";
+
+import "@/i18n";
+import "@/index.css";
+
+const root = ReactDOM.createRoot(
+    document.querySelector(".root")!
+);
+
+function App() {
+    useEffect(() => {
+        removeDefaultConsentLink();
+    }, []);
+
+    return <PageWrapper>
+        <Faq/>
+    </PageWrapper>;
+}
+
+root.render(
+    <I18nGate>
+        <App/>
+    </I18nGate>
+);
