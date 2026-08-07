a48403ca340c3bff804b6203ea5b833c137f5fe6
Manuel García Villaescusa <manuel.garcia.villaescusa@gmail.com>
2026-08-07 12:03:12 +0200

    Limita los estilos específicos al entrypoint de Puzzles

--- client/src/apps/features/puzzles/index.tsx
+++ client/src/apps/features/puzzles/index.tsx
@@ -9,6 +9,8 @@ import Puzzles from "./pages/Puzzles";
 
 import "@/i18n";
 import "@/index.css";
+import "@/components/layout/PageWrapper/PuzzlesLightRepair.css";
+import "@/components/layout/PageWrapper/PuzzlesSetupPolish.css";
 import * as styles from "./index.module.css";
 
 const root = ReactDOM.createRoot(
