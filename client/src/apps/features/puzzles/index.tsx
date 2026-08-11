ff9867e0925311b4008ef29f6e28102a79114197
Manuel García Villaescusa <manuel.garcia.villaescusa@gmail.com>
2026-08-11 17:01:54 +0200

    Puzzles: carga rail final del workspace

--- client/src/apps/features/puzzles/index.tsx
+++ client/src/apps/features/puzzles/index.tsx
@@ -16,6 +16,7 @@ import "@/index.css";
 import "@/components/layout/PageWrapper/PuzzlesLightRepair.css";
 import "@/components/layout/PageWrapper/PuzzlesSetupPolish.css";
 import "@/components/layout/PageWrapper/PuzzlesBoardFirst.css";
+import "@/components/layout/PageWrapper/PuzzlesWorkspaceRails.css";
 import * as styles from "./index.module.css";
 
 const root = ReactDOM.createRoot(
