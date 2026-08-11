eaf29c8d13980ecaf240fbe9274eea1b0a3c29e8
Manuel García Villaescusa <manuel.garcia.villaescusa@gmail.com>
2026-08-11 18:57:55 +0200

    Puzzles: carga pulido V4

--- client/src/apps/features/puzzles/index.tsx
+++ client/src/apps/features/puzzles/index.tsx
@@ -17,6 +17,7 @@ import "@/components/layout/PageWrapper/PuzzlesLightRepair.css";
 import "@/components/layout/PageWrapper/PuzzlesSetupPolish.css";
 import "@/components/layout/PageWrapper/PuzzlesFinalLayout.css";
 import "@/components/layout/PageWrapper/PuzzlesFinalFixes.css";
+import "@/components/layout/PageWrapper/PuzzlesPolishV4.css";
 import * as styles from "./index.module.css";
 
 const root = ReactDOM.createRoot(
