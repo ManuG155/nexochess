201e6b6ab52f5f95634d0c411dca9ae8f3dc6be8
Manuel García Villaescusa <manuel.garcia.villaescusa@gmail.com>
2026-08-21 12:44:07 +0200

    Remove orange teaching arrows from review

--- client/src/apps/features/analysis/pages/Analysis/BoardArea/index.tsx
+++ client/src/apps/features/analysis/pages/Analysis/BoardArea/index.tsx
@@ -13,7 +13,6 @@ import playBoardSound from "@/lib/boardSounds";
 
 import useEvaluation from "./useEvaluation";
 import useSuggestionArrows from "./useSuggestionArrows";
-import useTeachingArrows from "./useTeachingArrows";
 import * as styles from "./BoardArea.module.css";
 
 function BoardArea() {
@@ -41,16 +40,6 @@ function BoardArea() {
 
     const evaluation = useEvaluation();
     const suggestionArrows = useSuggestionArrows();
-    const teachingArrows = useTeachingArrows();
-    const reviewArrows = [
-        ...suggestionArrows.filter(suggestion => (
-            !teachingArrows.some(teaching => (
-                teaching.from == suggestion.from
-                && teaching.to == suggestion.to
-            ))
-        )),
-        ...teachingArrows
-    ];
     const flipBoardLabel = t("optionsToolbar.flipBoard");
 
     function addMove(move: Move) {
@@ -111,7 +100,7 @@ function BoardArea() {
             node={currentStateTreeNode}
             flipped={boardFlipped}
             evaluation={evaluation}
-            arrows={gameAnalysisOpen ? reviewArrows : []}
+            arrows={gameAnalysisOpen ? suggestionArrows : []}
             piecesDraggable={
                 gameAnalysisOpen
                 && analysisStatus == AnalysisStatus.INACTIVE
